7f3354a885e29ab45c05a880144389b6343eca22
root <root@nileshpadwal4561.mylabserver.com>
2018-02-04 05:19:01 +0000

    Added nginx

--- manifests/nginx.pp
+++ manifests/nginx.pp
@@ -1,10 +1,14 @@
-class profile::nginx {
-
-
- include ::nginx
-
- ::nginx::vhost{$facts['fqdn']:
-
- }
-
+class profile::nginx(
+  Array $vhosts,
+) {
+  include ::nginx
+  notify { "testdata":
+    message => $vhosts;
+  }
+  $vhosts.each |$vhost| {
+    ::nginx::vhost { $vhost['fqdn']:
+      port            => $vhost['port'],
+      server_aliases  => $vhost['server_aliases']
+    }
+  }
 }
